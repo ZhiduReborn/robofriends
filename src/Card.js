1d10de7743435848bd881e78194dbdcf181ba96f
ZhiduReborn <zhiduyan@gmail.com>
2019-02-23 17:59:42 -0600

    Robo1

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return(
+        <div>
+            <img alt='robots' src='' />
+            <div>
+                <h2>Name</h2>
+                <p>email</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
