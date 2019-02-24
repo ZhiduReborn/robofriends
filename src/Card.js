0851e9de9a07c84fe319b8ccdcb8fd74e3559192
ZhiduReborn <zhiduyan@gmail.com>
2019-02-23 18:02:19 -0600

    robo2

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = () => {
     return(
         <div>
-            <img alt='robots' src='' />
+            <img alt='robots' src='https://robohash.org/' />
             <div>
                 <h2>Name</h2>
                 <p>email</p>
