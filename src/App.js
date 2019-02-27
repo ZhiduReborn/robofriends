646c8637f744d4472176990ec8e967550a6fc3a7
ZhiduReborn <zhiduyan@gmail.com>
2019-02-26 20:46:03 -0600

    search robots

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import {robots} from './robots';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+
+    }
+
+    render() {
+        const fiteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        });
+        return (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={fiteredRobots} />
+            </div>
+        );
+    }
+}
+export default App;
