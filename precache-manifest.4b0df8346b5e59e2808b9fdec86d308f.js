d18030b7efd0e8c4b8f41c991576d35e5c60a1ed
ZhiduReborn <zhiduyan@gmail.com>
2019-03-03 15:18:04 -0600

    Updates

---
+++ precache-manifest.4b0df8346b5e59e2808b9fdec86d308f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "342fd53ce0cd20372011",
+    "url": "/robofriends/static/js/main.342fd53c.chunk.js"
+  },
+  {
+    "revision": "e519322fae5251e380f1",
+    "url": "/robofriends/static/js/1.e519322f.chunk.js"
+  },
+  {
+    "revision": "342fd53ce0cd20372011",
+    "url": "/robofriends/static/css/main.9b75c89b.chunk.css"
+  },
+  {
+    "revision": "e519322fae5251e380f1",
+    "url": "/robofriends/static/css/1.36221255.chunk.css"
+  },
+  {
+    "revision": "45fb2026bd619b3a6d3b91e48de3f0b7",
+    "url": "/robofriends/index.html"
+  }
+];
